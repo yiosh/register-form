1233f7086249a800427570a153332592be81f9db
Josue Daniel Guerrero Ballestero <iinoted@gmail.com>
2019-08-26 15:17:57 +0200

    edit: change endpoint for horizontal directory

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = {
   //   disableHostCheck: true
   // },
   publicPath:
-    process.env.NODE_ENV === "production" ? "/v4/fl_app/registerForm/" : "/",
+    process.env.NODE_ENV === "production" ? "/v4/fl_app/registerFormH/" : "/",
   indexPath: "index.php"
 };
