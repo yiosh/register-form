c80f965364878fea5b31fad481b06dd18a0b8ffe
Josue Daniel Guerrero Ballestero <iinoted@gmail.com>
2019-08-26 15:18:29 +0200

    edit: hide title and logo from header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app id="inspire">
     <v-toolbar app color="white">
-      <v-toolbar-title class="headline text-uppercase">
+      <!-- <v-toolbar-title class="headline text-uppercase">
         <span>{{register}}</span>
         <span class="font-weight-light">su</span>
-      </v-toolbar-title>
+      </v-toolbar-title> -->
 
-      <v-img
+      <!-- <v-img
         src="https://secure.1x2live.it/fl_config/secure.1x2live.it/img/logo.jpg"
         max-height="100%"
         contain
         style="margin: 0 auto"
-      ></v-img>
+      ></v-img> -->
 
       <v-toolbar-items></v-toolbar-items>
     </v-toolbar>
